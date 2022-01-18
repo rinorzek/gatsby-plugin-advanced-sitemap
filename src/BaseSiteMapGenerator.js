73c4f64814471f6843901fb6ce4074d1655c4035
Rinor Zekaj <78733927+rinorzek@users.noreply.github.com>
2022-01-18 14:04:48 +0100

    fix: adds attribute for xhtml in urlset

--- src/BaseSiteMapGenerator.js
+++ src/BaseSiteMapGenerator.js
@@ -9,7 +9,8 @@ import * as utils from './utils';
 const XMLNS_DECLS = {
     _attr: {
         xmlns: `http://www.sitemaps.org/schemas/sitemap/0.9`,
-        'xmlns:image': `http://www.google.com/schemas/sitemap-image/1.1`
+        'xmlns:image': `http://www.google.com/schemas/sitemap-image/1.1`,
+        'xmlns:xhtml': `http://www.w3.org/1999/xhtml`
     }
 };
 
